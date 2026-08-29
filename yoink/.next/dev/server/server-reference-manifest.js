45585c8f370a46209e2a1cc6ac44c5dd9285ec45
rynking <ryannyagah6@gmail.com>
2026-08-30 02:34:24 +1000

    wip: messaging

--- yoink/.next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"3qBJ2NhPQzzMROcRlOyR7Iwh8zdlG340n9ct6lHv9+4=\"\n}"
--- yoink/.next/dev/static/chunks/_15rhqqh._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/[root-of-the-server]__04kpziu._.css","static/chunks/node_modules_next_dist_20wefz_._.js","static/chunks/node_modules_0vhk5f_._.js","static/chunks/src_0is7tn2._.js"],
-    source: "entry"
-});
